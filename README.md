1a4d09f0042365e23cc38d0af3782e0d8ee998db
EdOverride <EdOverride@gmail.com>
2019-08-18 01:12:13 +0300

    Add visual theme & update theme

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 #### My version of Darcula theme for VS Code, inspired by [JetBrains IDE](https://www.jetbrains.com), with some personal touches.
 
+
 ## Languages included:
 * Ruby
 * JavaScript
@@ -15,6 +16,34 @@
 * XML
 * Python
 
+
+## Code Highlight
+
+<img src="https://raw.githubusercontent.com/EdOverride/Full-Darcula-Theme/master/examples/code_scss.png" height="400px">
+
+##### More code syntax highligh examples in [Examples](examples) folder.
+
+
+## Editor Theme
+
+Theme also comes with dark visual styles with two different styles,
+ with background image:
+
+![](https://raw.githubusercontent.com/EdOverride/Full-Darcula-Theme/master/examples/1.png)
+
+or without:
+
+![](https://raw.githubusercontent.com/EdOverride/Full-Darcula-Theme/master/examples/2.png)
+
+To activate/deactivate background image, open Command Palette (default: `ctrl+shift+p`)
+and execute commands:
+
+* Activate Background Image: `Full Darcula Theme: Activate Background Image`
+* Deactivate Background Image: `Full Darcula Theme: Deactivate Background Image`
+
+# ⚠️ **Warning**: If you have enabled background image, you must disable it, before uninstalling extension, or it will stay active.
+
+
 ## TODO's:
 * Add languages:
   * Java
@@ -25,8 +54,10 @@
   * VB.Net
   * VBS
 
+
 ## How to Debug
 Press CTRL+SHIFT+P and type `Developer: Inspect TM Scopes`
+
 
 ## How to generate package file
 ```bash
@@ -40,6 +71,7 @@ npm install -g vsce
 vsce package
 vsce publish
 ```
+
 
 #### Most likely it's not perfect/complete. Feel free to submit suggestions on what's missing.
 
---
+++ extension.js
@@ -0,0 +1,36 @@
+﻿const vscode = require('vscode');
+const path   = require('path');
+const fs     = require('fs');
+
+const install_dir = path.dirname(require.main.filename);
+const css_name    = (vscode.version >= "1.38") ? 'workbench.desktop.main.css' : 'workbench.main.css';
+const css_path    = path.join(install_dir, 'vs', 'workbench', css_name);
+const ext_path    = vscode.extensions.getExtension('EdOverride.full-darcula-theme').extensionPath;
+const image_path  = path.join(ext_path, 'themes', 'bg1.jpg').replace(/\\+/g, '/')
+const css_regex   = new RegExp(/(\/\*full-darcula-theme-start\*\/.+\/\*full-darcula-theme-end\*\/)/g);
+const css         = `/*full-darcula-theme-start*/body{background-repeat:repeat;background-image:url('${image_path}');opacity:0.72;background-size: 550px;}/*full-darcula-theme-end*/`
+
+function activate(context) {
+
+  let activate_bg = vscode.commands.registerCommand('full-darcula-theme-activate-bg', function () {
+    let content = fs.readFileSync(css_path, 'utf-8');
+    content     = content.replace(css_regex, '');
+    content     = content + css;
+    fs.writeFileSync(css_path, content, 'utf-8');
+    vscode.commands.executeCommand('workbench.action.reloadWindow');
+  });
+
+  let deactivate_bg = vscode.commands.registerCommand('full-darcula-theme-deactivate-bg', function () {
+    let content = fs.readFileSync(css_path, 'utf-8');
+    content     = content.replace(css_regex, '');
+    fs.writeFileSync(css_path, content, 'utf-8');
+    vscode.commands.executeCommand('workbench.action.reloadWindow');
+  });
+
+  context.subscriptions.push(activate_bg, deactivate_bg);
+}
+
+function deactivate() { }
+
+exports.activate = activate;
+module.exports = {activate, deactivate};
